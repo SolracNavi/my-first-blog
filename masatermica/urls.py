1222cd9e82a68bb43c5d8ce2ed81f532e5a1c9ed
Carlos Ponticelli <el.sariri@gmail.com>
2019-07-31 20:55:21 -0300

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- masatermica/urls.py
+++ masatermica/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
 
